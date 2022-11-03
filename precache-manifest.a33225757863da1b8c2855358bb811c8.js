d1c152eda8dbd025ca5eeb59c4c733420801e2c0
agonzale110 <agonzale110@cuc.edu.co>
2022-11-03 16:23:36 -0500

    Deploy build

---
+++ precache-manifest.a33225757863da1b8c2855358bb811c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b02eb253e9629c5445",
+    "url": "./static/css/main.e0218bd7.chunk.css"
+  },
+  {
+    "revision": "f3b02eb253e9629c5445",
+    "url": "./static/js/main.f3b02eb2.chunk.js"
+  },
+  {
+    "revision": "d2fd5e860431f1be0b24",
+    "url": "./static/js/1.d2fd5e86.chunk.js"
+  },
+  {
+    "revision": "17a9301b537403d8cada",
+    "url": "./static/css/2.615d217b.chunk.css"
+  },
+  {
+    "revision": "17a9301b537403d8cada",
+    "url": "./static/js/2.17a9301b.chunk.js"
+  },
+  {
+    "revision": "a02153e44480a5052a3e",
+    "url": "./static/js/runtime~main.a02153e4.js"
+  },
+  {
+    "revision": "3c3374cef138d0546bdc71541c33ff93",
+    "url": "./index.html"
+  }
+];
